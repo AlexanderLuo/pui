955a4b8eb3e13af0287977b435a95f25f8b86973
Alexander <496952252@qq.com>
2018-12-14 12:38:38 +0800

    kill -th

---
+++ packages/tag/src/tag.vue
@@ -0,0 +1,32 @@
+<template>
+
+  <span :class="['p-tag']"
+        :style="{backgroundColor:color}"
+  >
+    <slot></slot>
+
+    <i v-if="closable" class="p-tag__close el-icon-close" @click="handleClose"></i>
+
+  </span>
+</template>
+
+<script>
+	export default {
+		name: "PTag",
+    props: {
+		  text: String,
+      color: String,
+      closable: false
+    },
+    methods:{
+		  handleClose(evt){
+
+      }
+    },
+	}
+</script>
+
+<style scoped>
+
+
+</style>
